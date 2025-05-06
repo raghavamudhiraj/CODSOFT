eca660bdfb2d5b1c793b1d24c45ca9284e71cdf9
raghavamudhiraj <raghavamudhiraj23@gmail.com>
2025-05-06 23:42:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,45 @@
+// Student.java
+public class Student {
+    private String name;
+    private int rollNumber;
+    private String grade;
+
+    // Constructor
+    public Student(String name, int rollNumber, String grade) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    // Override toString for easy display of student info
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + rollNumber + ", Grade: " + grade;
+    }
+}
